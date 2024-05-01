871f218d7ad334bfd2005b88c0af58f2cde4a460
Han_Han <2088113262@qq.com>
2024-05-01 17:40:01 +0800

    完成单机伪分布式，多进程

--- python-pseudo/main.py
+++ python-pseudo/main.py
@@ -1,10 +1,7 @@
-import glob
-
 from mapper import Mapper
 from reducer import Reducer
 import pickle
-import queue
-from collections import defaultdict, OrderedDict
+from collections import defaultdict
 import multiprocessing
 import sys
 
@@ -20,19 +17,19 @@ def read_file(file_path) -> list:
         return lines
 
 
-def divide_file(content, filename):
+def divide_file(content, filenames):
     """
     把文件均匀分成n份，每份送入一个map/reduce，能处理不能均分的情况
     :param content: 输入文件内容
-    :param filename: 保存文件的名字前缀
+    :param filenames: 要保存的文件的名字
     """
     # 每组map传入的输入数据数量为 文件行数//进程数
     total_lines = len(content)
     lines_per_file = total_lines // num_workers
     extra_lines = total_lines % num_workers
 
-    for i in range(num_workers):
-        with open(f"../{filename}_{i}.txt", 'w') as f:
+    for i in range(len(filenames)):
+        with open(filenames[i], 'w') as f:
             start_line = i * lines_per_file + min(i, extra_lines)  # 对于多余的行数，在前面文件中每个加一行
             end_line = start_line + lines_per_file + (1 if i < extra_lines else 0)
             f.writelines(content[start_line:end_line])
@@ -41,8 +38,8 @@ def divide_file(content, filename):
 def worker_map(map_input: str, map_output: str):
     """
     map方法，将输入解析为 (xx, 1) 的形式
-    :param input_path: ["a bb", "bb cc"]
-    :param output_path: [('a', 1), ('bb', 1), ('bb', 1), ('cc', 1)]
+    :param map_input: ["a bb", "bb cc"]
+    :param map_output: [('a', 1), ('bb', 1), ('bb', 1), ('cc', 1)]
     """
     with open(map_output, 'w') as f:
         for line in read_file(map_input):
@@ -51,14 +48,13 @@ def worker_map(map_input: str, map_output: str):
                 f.write(f'{k}\t{v}\n')
 
 
-def shuffle(file_pattern):
+def shuffle():
     """
     对map的输出文件进行shuffle和排序后输出，输出为多个reduce输入文件
-    :param file_pattern: map的输出文件格式，如："../map_output_*.txt"
     """
     shuffle_data = defaultdict(list)
     # 读取每个文件并将键值对添加到字典中
-    for filename in glob.glob(file_pattern):
+    for filename in map_output_paths:
         with open(filename, 'r') as f:
             for line in f:
                 key, value = line.strip().split('\t')
@@ -66,17 +62,17 @@ def shuffle(file_pattern):
     # 将字典内容排序并输出为字符串数组
     shuffle_data_str = [key+'\t'+str(shuffle_data[key])+'\n' for key in sorted(shuffle_data)]
     # 拆分数据
-    divide_file(shuffle_data_str, "reduce_input")
+    divide_file(shuffle_data_str, reduce_input_paths)
 
 
-def worker_reduce(redece_input: str, reduce_output: str):
+def worker_reduce(reduce_input: str, reduce_output: str):
     """
     reduce方法，对shuffle的结果求和输出
-    :param redece_input: [('a', [1]), ('bb', [1, 1]), ('cc', [1])]
+    :param reduce_input: [('a', [1]), ('bb', [1, 1]), ('cc', [1])]
     :param reduce_output: [('a', 1), ('bb', 2), ('cc', 1)]
     """
     with open(reduce_output, 'w') as f:
-        for line in read_file(redece_input):
+        for line in read_file(reduce_input):
             key, values = line.strip().split('\t')
             k, v = reducer.reduce(key, eval(values))
             f.write(f'{k}\t{v}\n')
@@ -97,12 +93,17 @@ if __name__ == '__main__':
 
     # map/reduce进程数量
     num_workers = 3
+    # 各阶段文件存储地址
+    map_input_paths =     [f"data/map_input_{i}.txt"     for i in range(num_workers)]
+    map_output_paths =    [f"data/map_output_{i}.txt"    for i in range(num_workers)]
+    reduce_input_paths =  [f"data/reduce_input_{i}.txt"  for i in range(num_workers)]
+    reduce_output_paths = [f"data/reduce_output_{i}.txt" for i in range(num_workers)]
 
-    # 拆分输入为 num_workers 个文件，
-    divide_file(read_file(input_path), "map_input")
+    # 拆分输入为 num_workers 个文件
+    divide_file(read_file(input_path), map_input_paths)
 
     # 创建并启动Mapper进程
-    map_processes = [multiprocessing.Process(target=worker_map, args=(f"../map_input_{i}.txt", f"../map_output_{i}.txt"))
+    map_processes = [multiprocessing.Process(target=worker_map, args=(map_input_paths[i], map_output_paths[i]))
                      for i in range(num_workers)]
     for p in map_processes:
         p.start()
@@ -110,10 +111,10 @@ if __name__ == '__main__':
         p.join()
 
     # shuffle数据
-    shuffle("../map_output_*.txt")
+    shuffle()
 
     # 创建并启动Reducer进程
-    reduce_processes = [multiprocessing.Process(target=worker_reduce, args=(f"../reduce_input_{i}.txt", f"../reduce_output_{i}.txt"))
+    reduce_processes = [multiprocessing.Process(target=worker_reduce, args=(reduce_input_paths[i], reduce_output_paths[i]))
                         for i in range(num_workers)]
     for p in reduce_processes:
         p.start()
@@ -122,7 +123,7 @@ if __name__ == '__main__':
 
     # 合并reduce结果
     with open(output_path, 'w') as f:
-        for i in range(num_workers):
-            with open(f"../reduce_output_{i}.txt", 'r') as rf:
+        for filename in reduce_output_paths:
+            with open(filename, 'r') as rf:
                 for s in rf:
                     f.write(s)
